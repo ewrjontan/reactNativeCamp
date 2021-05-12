683aefdbe2924df9be555124da22965d8aff51d0
Jonathan Cardenas <jonscardenas@gmail.com>
2021-05-12 09:32:48 -0500

    week 1 navigation in react exercise 1 and 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList, } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props){
+class Directory extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
 
-    const renderDirectoryItem = ({item}) => {
+    //static keyword sets method on class rather than object. Communicates title to directory navigators navigation options
+    static navigationOptions = {
+        title: 'Directory'
+    };
+    
+    render(){
+        const { navigate } = this.props.navigation;
+
+
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                />
+            );
+        };
+
         return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
+            <FlatList 
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+
             />
         );
-    };
-
-    return (
-        <FlatList 
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-
-        />
-    );
+    }
 }
 
 export default Directory;
