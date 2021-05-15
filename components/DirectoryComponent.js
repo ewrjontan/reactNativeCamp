4cb79a9170ab1b8ccfb0ea1a9368e1b6ce82a38b
Jonathan Cardenas <jonscardenas@gmail.com>
2021-05-15 12:37:53 -0500

    workshop 1 task 3 done

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, } from 'react-native';
+import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 
