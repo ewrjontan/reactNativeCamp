97d545f8f709481fedd7701d9c08784575e1848e
Jonathan Cardenas <jonscardenas@gmail.com>
2021-05-15 11:47:28 -0500

    workshop 1 task 2 done

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 //import { FlatList, } from 'react-native';
 //import { ListItem } from 'react-native-elements';
 //import { CAMPSITES } from '../shared/campsites';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
 
 
 class Contact extends Component {
@@ -21,6 +23,20 @@ class Contact extends Component {
     
         return (
             <ScrollView>
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}
+                >
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+
+                    
+
+                </Card>
 
             </ScrollView>
         );
